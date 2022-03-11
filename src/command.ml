37cabfb24ba1311a1f3e413ed274d2834797e298
Daniel Jin <dj263@cornell.edu>
2022-03-11 16:46:31 -0500

    Implement command.mli

---
+++ src/command.ml
@@ -0,0 +1,23 @@
+type command = 
+  | Post
+  | HomePage
+  | Search
+  | ViewProfile
+  | DeleteMyPost
+
+exception Empty
+exception Invalid
+
+let remove_whitespace lst = List.filter (fun x -> x <> "") lst
+
+(** Convert user input into a command. *)
+let parse str = 
+  let text =  String.lowercase_ascii str in
+  let txt_lst = String.split_on_char ' ' str in
+  match remove_whitespace txt_lst with
+  | [ "post" ] -> Post
+  | [ "homepage" ] -> Post
+  | [ "search" ] -> Search
+  | [ "viewprofile" ] -> ViewProfile
+  | [ "deletemypost" ] -> DeleteMyPost
+  | _ -> raise Invalid

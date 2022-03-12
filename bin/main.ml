5a044c88a1b30ba3df1e7cfc44608d46aaade2fd
Purit <pb465@cornell.edu>
2022-03-12 12:11:31 -0500

    Set up user interface

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,27 @@
 open Twitter
-open Posts
 
-let print_blue s = ANSITerminal.print_string [ ANSITerminal.blue ] s
+(* open Posts *)
+open Command
 
-let post =
-  print_blue "Enter a post:";
-  match read_line () with
-  | str -> create_post str
+let print_blue s =
+  ANSITerminal.print_string [ ANSITerminal.cyan ] (s ^ "\n")
+
+(* let rec get_command = print_endline "Please enter a command.\n";
+   print_string "> "; try match parse (read_line ()) with | Post ->
+   failwith "not implemented" | Quit -> print_blue "See you next
+   time!\n"; exit 0 with Failure _ -> print_blue "Invalid command"; *)
+
+let post s = failwith s
 
 let main () =
   print_blue "\nWelcome to Twitter.\n";
-  post
+  print_blue "\nWhat would you like to do?\n";
+  try
+    match parse (read_line ()) with
+    | Post s -> post s
+    | Quit ->
+        print_blue "\nSee you next time!\n";
+        exit 0
+  with Failure _ -> ()
+
+let () = main ()

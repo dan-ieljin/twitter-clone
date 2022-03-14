06b766be9de5cf1eb2d0785c59be2cc85b430b25
Joshua Huang <jth239@cornell.edu>
2022-03-13 23:14:59 -0400

    Purit's main.ml edit

--- bin/main.ml
+++ bin/main.ml
@@ -5,27 +5,42 @@ open Command
 let print_blue s =
   ANSITerminal.print_string [ ANSITerminal.cyan ] (s ^ "\n")
 
-(* let rec get_command = print_endline "Please enter a command.\n";
-   print_string "> "; try match parse (read_line ()) with | Post ->
-   failwith "not implemented" | Quit -> print_blue "See you next
-   time!\n"; exit 0 with Failure _ -> print_blue "Invalid command"; *)
+let rec post s =
+  create_post s [] 0 |> json_output |> to_json;
+  print_blue "\nWhat would you like to do?\n";
+  try parse (read_line ()) |> get_command
+  with Invalid | Empty ->
+    print_blue "Invalid command. Please enter a new command.\n"
 
-let post s = create_post s [] 0 |> json_output |> to_json
+and get_command s =
+  match s with
+  | Post s ->
+      print_blue "\nPost successful!\n";
+      post s
+  | HomePage ->
+      print_blue "\nMain Feed:\n";
+      Yojson.Basic.pretty_print Format.std_formatter
+        (Yojson.Basic.from_file "data/posts.json")
+  | Quit ->
+      print_blue "See you next\n  time!\n";
+      exit 0
 
 let main () =
   print_blue "\nWelcome to Twitter.\n";
   print_blue "\nWhat would you like to do?\n";
   let posts = Yojson.Basic.from_file "data/posts.json" in
-
   try
     match parse (read_line ()) with
-    | Post s -> post s
+    | Post s ->
+        print_blue "\nPost successful!\n";
+        post s
     | HomePage ->
         print_blue "\nMain Feed:\n";
         Yojson.Basic.pretty_print Format.std_formatter posts
     | Quit ->
-        print_blue "\nSee you next time!\n";
+        print_blue "\nSee you next time!";
         exit 0
-  with Failure _ -> ()
+  with Invalid | Empty ->
+    print_blue "Invalid command. Please enter a new command.\n"
 
 let () = main ()

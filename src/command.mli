b8d8f058ba076f5fe0eee3e423fe287c079f5998
Purit <pb465@cornell.edu>
2022-05-17 20:39:33 -0400

    added documentation

--- src/command.mli
+++ src/command.mli
@@ -1,5 +1,5 @@
-(** Parsing of user input into commands. *)
-
+(** The type [command] represents commands that users enter into the
+    program *)
 type command =
   | Post
   | HomePage
@@ -30,15 +30,21 @@ type command =
   | AnswerPoll of int
   | Help
 
+(** The type [sort_command] represents the commands that users enter to
+    sort posts. *)
 type sort_command =
   | Newest
   | Oldest
   | Likes
 
 exception Empty
+(** Raised when an empty command is parsed. *)
+
 exception Invalid
+(** Raised when an invalid command is parsed. *)
 
 val parse : string -> command
 (** Convert user input into a command. *)
 
 val parse_sort : string -> sort_command
+(** Converts user input for sorting posts into a command. *)
